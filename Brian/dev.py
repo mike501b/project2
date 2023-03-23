6fabc265ed574677cc61b71288ecc68ac4e412e4
brianwander <brianwander101@gmail.com>
2023-03-22 20:16:28 -0400

    Modified Alpaca API call function

--- Brian/dev.py
+++ Brian/dev.py
@@ -1,33 +1,17 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "id": "ee6135ab-e805-4a02-8205-2389a9db8027",
-   "metadata": {},
-   "outputs": [],
-   "source": []
-  }
- ],
- "metadata": {
-  "kernelspec": {
-   "display_name": "dev",
-   "language": "python",
-   "name": "dev"
-  },
-  "language_info": {
-   "codemirror_mode": {
-    "name": "ipython",
-    "version": 3
-   },
-   "file_extension": ".py",
-   "mimetype": "text/x-python",
-   "name": "python",
-   "nbconvert_exporter": "python",
-   "pygments_lexer": "ipython3",
-   "version": "3.7.15"
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 5
-}
+from os import getenv
+from dotenv import load_dotenv
+from alpaca_trade_api.rest import REST, TimeFrame, TimeFrameUnit
+import pandas as pd
+from datetime import date, timedelta
+
+def get_bars_alpaca(start_date=str(date.today()-timedelta(days=365*5)),end_date=str(date.today()-timedelta(days=1)),ticker='SOYB'):
+    load_dotenv()
+    alpaca_api_key=getenv('ALPACA_API_KEY')
+    secret_key=getenv('ALPACA_SECRET_KEY')
+    if type(alpaca_api_key) == 0:
+        raise Exception("No ALPACA API KEY was loaded. Please save a .env file containing an ALPACA_API_KEY in the local directory.")
+    if type(secret_key) == 0:
+        raise Exception("No ALPACA SECRET KEY was loaded. Please save a .env file containing an ALPACA_SECRET_KEY in the local directory.")
+    api=REST(key_id=alpaca_api_key,secret_key=secret_key)
+    df=api.get_bars("SOYB", TimeFrame.Day, start_date, end_date, adjustment='raw').df
+    return(df)
